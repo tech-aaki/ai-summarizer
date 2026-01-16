2e916fa04247dbf234ed9d96db4f564d4cd6b199
tech-aaki <aakash@techandhub.in>
2026-01-16 17:14:43 +0530

    Delete extension directory

--- extension/content.js
+++
@@ -1,6 +0,0 @@
-chrome.runtime.onMessage.addListener((req, sender, sendResponse) => {
-  if (req.action === "getText") {
-    const text = document.body.innerText;
-    sendResponse({ text });
-  }
-});
